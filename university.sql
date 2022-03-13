3c67d3a4d7dd9b6cb16fc7383b6b83c9fa6f0b86
Danoz25 <daniel.zakosek.702@my.csun.edu>
2022-03-12 22:26:45 -0800

    sql schema done

--- university.sql
+++ university.sql
@@ -1,10 +1,37 @@
--- Before execute the file, ADD your database name here:
+-- Before execute the file, ADD your database name here: projdb
 -- The database name should be the same as your database of your user table from step 1
 use `projdb`; 
 
 --
 -- Table structure for table `department`
 --
+
+DROP TABLE IF EXISTS `user`;
+-- /*!40101 SET @saved_cs_client     = @@character_set_client */;
+-- /*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user` (
+  `username` varchar(10) NOT NULL,
+  `password` varchar(45) NOT NULL,
+  `firstname` varchar(45) DEFAULT NULL,
+  `lastname` varchar(45) DEFAULT NULL,
+  `email` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`username`),
+  UNIQUE KEY `email_UNIQUE` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+ -- /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+-- /*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES ('csun1','csun1','fname1','lname1','csun1@csun.edu');
+-- /*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+
+-- Dump completed on 2022-03-12 22:17:49
+
 DROP TABLE IF EXISTS `department`;
 CREATE TABLE `department` (
   `dept_name` varchar(20) COLLATE utf8mb4_general_ci NOT NULL,

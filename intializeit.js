188915aaa528410a5652ec2eb6f10b1274b9013f
Danoz25 <daniel.zakosek.702@my.csun.edu>
2022-03-13 01:18:00 -0800

    redo

--- intializeit.js
+++ intializeit.js
@@ -1,6 +1,32 @@
+const mysql = require('mysql');
+const express = require('express');
 
 
-function StartDatabase()
-{
-    
-}
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: ''
+});
+
+db.connect(err =>{
+    if(err){
+        throw err
+    }
+    console.log('MySQL connected')
+});
+
+const app = express()
+
+app.get('/createdb', (req, res) => {
+    let sql = 'CREATE DATABASE projdb'
+    db.query(sql, err => {
+        if(err){
+            throw err
+        }
+        res.send('Database Created');
+    });
+});
+
+app.listen('3000', () => {
+    console.log('Server Started on Port 3000')
+});

694abba768dfc6c7e80222d2aeebce0235b51b9b
Danoz25 <daniel.zakosek.702@my.csun.edu>
2022-03-12 23:03:18 -0800

    sql and most regisert and login button should work

--- Login.js
+++ Login.js
@@ -5,6 +5,7 @@ const express = require('express');
 const session = require('express-session');
 const path = require('path');
 
+
 function openRegisterForm()
 {
     const registerbtn = document.getElementById("register").onclick = function ()
@@ -17,16 +18,40 @@ function openRegisterForm()
 
 function LoginHere()
 {
-    const loginbtn = document.getElementById("log").onclick = function ()
+    const loginbtn = document.getElementById("log").onclick = function()
     {
 
-    };
+    
+    var express = require('express');
+    var router = express.Router();
+    var db=require('../database');
+    /* GET users listing. */
+    router.get('/login', function(req, res, next) {
+      res.render('login-form');
+    });
+    
+    router.post('/login', function(req, res){
+        var emailAddress = req.body.email_address;
+        var password = req.body.password;
+    
+        var sql='SELECT * FROM registration WHERE email_address =? AND password =?';
+        db.query(sql, [emailAddress, password], function (err, data, fields) {
+            if(err) throw err
+            if(data.length>0){
+                req.session.loggedinUser= true;
+                req.session.emailAddress= emailAddress;
+                res.redirect('/dashboard');
+            }else{
+                res.render('login-form',{alertMsg:"Your Email Address or password is wrong"});
+            }
+        })
+    
+    })
+    
+    module.exports = router;
+    location.href = "http://127.0.0.1:5500/Initialize.html";
+    }
+
+
 
 }
-const connection = mysql.createConnection({
-	host     : 'localhost',
-	user     : 'root',
-	password : '',
-	database : 'nodelogin'
-});
-
---
+++ register.js
@@ -0,0 +1,50 @@
+
+
+var express = require('express');
+var router = express.Router();
+var db=require('../university.sql');
+
+function Completeregi()
+{
+
+
+const submitregister = document.getElementById("complete").onclick = function()
+
+{
+// to display registration form 
+router.get('/register', function(req, res, next) {
+  res.render('registration-form');
+});
+
+// to store user input detail on post request
+router.post('/register', function(req, res, next) {
+    
+    inputData ={
+        username: req.body.username,
+        password: req.body.password,
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email
+    }
+// check unique email address
+    var sql='SELECT * FROM registration WHERE email_address =?';
+    db.query(sql, [inputData.email] ,function (err, data, fields) {
+    if(err) throw err
+    if(data.length>1){
+         var msg = inputData.email_address+ "was already exist";
+    }else{
+     
+    // save users data into database
+    var sql = 'INSERT INTO registration SET ?';
+   db.query(sql, inputData, function (err, data) {
+      if (err) throw err;
+           });
+  var msg ="Your are successfully registered";
+ }
+ res.render('registration-form',{alertMsg:msg});
+})
+     
+});
+        module.exports = router;
+    }
+}

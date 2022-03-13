db5cb20646c672938b2c42ff8fc1828756ea1562
Danoz25 <daniel.zakosek.702@my.csun.edu>
2022-03-13 00:55:39 -0800

    update

--- Login.js
+++ Login.js
@@ -1,10 +1,36 @@
-const loginbtn = document.getElementById("log");
-const registerbtn = document.getElementById("register");
 const mysql = require('mysql');
 const express = require('express');
-const session = require('express-session');
 const path = require('path');
 
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: ''
+});
+
+db.connect(err =>{
+    if(err){
+        throw err
+    }
+    console.log('MySQL connected')
+});
+
+const app = express()
+
+app.get('/createdb', (req, res) => {
+    let sql = 'CREATE DATABASE projdb'
+    db.query(sql, err => {
+        if(err){
+            throw err
+        }
+        res.send('Database Created');
+    });
+});
+
+app.listen('3000', () => {
+    console.log('Server Started on Port 3000')
+});
 
 function openRegisterForm()
 {
@@ -20,37 +46,10 @@ function LoginHere()
 {
     const loginbtn = document.getElementById("log").onclick = function()
     {
-
-    
-    var express = require('express');
-    var router = express.Router();
-    var db=require('../database');
-    /* GET users listing. */
-    router.get('/login', function(req, res, next) {
-      res.render('login-form');
-    });
-    
-    router.post('/login', function(req, res){
-        var emailAddress = req.body.email_address;
-        var password = req.body.password;
-    
-        var sql='SELECT * FROM registration WHERE email_address =? AND password =?';
-        db.query(sql, [emailAddress, password], function (err, data, fields) {
-            if(err) throw err
-            if(data.length>0){
-                req.session.loggedinUser= true;
-                req.session.emailAddress= emailAddress;
-                res.redirect('/dashboard');
-            }else{
-                res.render('login-form',{alertMsg:"Your Email Address or password is wrong"});
-            }
-        })
-    
-    })
-    
-    module.exports = router;
+        location.href = "http://127.0.0.1:5500/Initialize.html";
+   
     };
-    location.href = "http://127.0.0.1:5500/Initialize.html";
+  
 
 
 

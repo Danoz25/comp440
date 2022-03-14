b4841226715399ad9c678eefc82ee3b06d3816ee
Danoz25 <daniel.zakosek.702@my.csun.edu>
2022-03-14 00:39:40 -0700

    php files

--- mysqli_connect.php
+++ mysqli_connect.php
@@ -1,10 +1,15 @@
 <?php
+//all DB host name may change based off what yours says.
    define('DB_USER', 'comp440');
    define('DB_PASSWORD', 'pass1234');
-   define('DB_HOST','localhost');
-   define('DB_NAME','COMP440_PROJECTDB');
+   define('DB_HOST','127.0.0.1:3050');
+   define('DB_NAME','comp_440_projectdb');
 
-   $dbc = @mysqli_connect(DB_HOST,DB_USER,DB_PASSWORD,DB_NAME)
+
+
+
+
+   $dbc = @mysqli_connect(DB_HOST,DB_USER,DB_PASSWORD,DB_NAME,)
    OR die ('Could not connect to MySQL: ' .
     mysqli_connection_error());
 ?>

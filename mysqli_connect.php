3eca7b51ac1a81909d0ea9fab713e125d8726c38
Danoz25 <daniel.zakosek.702@my.csun.edu>
2022-03-14 14:54:01 -0700

    dwd

--- mysqli_connect.php
+++ mysqli_connect.php
@@ -1,15 +1,26 @@
+<?php
+//all DB host name may change based off what yours says.
+   define('DB_USER', 'comp440');
+   define('DB_PASSWORD', 'pass1234');
+   define('DB_HOST','127.0.0.1:3050');
+   define('DB_NAME','comp_440_projectdb');
 
-<?php 
-    // Enable us to use Headers
-    ob_start();
-    // Set sessions
-    if(!isset($_SESSION)) {
-        session_start();
+
+
+
+
+   $connection = @mysqli_connect(DB_HOST,DB_USER,DB_PASSWORD,DB_NAME)
+   OR die ('Could not connect to MySQL: ' .
+    mysqli_connect_error());
+
+    $query = "SELECT username, lastname FROM user";
+    $response = @mysqli_query($connection, $query);
+    if($response){
+        while($row=mysqli_fetch_array($response)){
+            echo $row['username'] . ' ' . $row['lastname'];
+        }
     }
-    $hostname = "127.0.0.1:3050";
-    $username = "comp440";
-    $password = "pass1234";
-    $dbname = "comp_440_projectdb";
-    
-    $connection = mysqli_connect($hostname, $username, $password, $dbname) or die("Database connection not established.")
+ 
+
+
 ?>

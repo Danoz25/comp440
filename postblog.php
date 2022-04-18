ad6668c1615f4f1a46694bc70ffb5d1d1226919a
xethr0 <jamesmaturino@gmail.com>
2022-04-18 01:09:06 -0700

    added new file (postblog.php)

---
+++ postblog.php
@@ -0,0 +1,102 @@
+<!doctype html>
+<html lang="en">
+    <body>
+        <h3>Blog Post</h3>
+        <div class="form-group">
+            <label>Subject</label>
+            <input type="text" class="form-control" name="subject" id="subject" />
+        </div>
+        <div class="form-group">
+            <label> Description</label>
+            <div></div>
+            <textarea id="description" name="description" rows = "7" style="width:100%; max_width=100%;"></textarea>
+        </div>
+        <div class="form-group">
+            <label>Tags</label>
+            <input type="tag" class="form-control" name="tag" id="tag" />
+        </div>
+        <div class="form-group">
+            <label>Submit</label>
+        </div>
+        <button type="submit" name="submit" id="submit" class="btn btn-outline-primary btn-lg btn-block" value = "Submit">
+    </body>
+</html>
+
+<?php
+
+    include('mysqli_connect.php');
+
+    
+
+    if(isset($_POST["submit"]))
+    {
+        $date = date("m/d/y");
+        $subject  = $_POST["subject"];
+        $username = $_POST["username"];
+        $description = $_POST["description"];
+        $tag_name = $_POST['tag'];
+
+        $subject_err = $description_err = $tag_error = "";
+
+        $foundTag = false;
+        
+
+        $tag_check_query = mysqli_query($connection, "SELECT tag_name FROM tags");
+        $tagRows = mysqli_fetch_all($tag_check_query, MYSQLI_ASSOC);
+                
+
+        if(!empty($subject) && !empty($description) && !empty($tag))
+        {
+            foreach($tagRows as $row)
+            {
+                if ($tag_name == $row["tag_name"])
+                {
+                    $foundTag = true;
+                }
+            }
+
+            if (!$foundTag)
+            {
+                $sqlTag = "INSERT INTO tags (tag_name) VALUES ('{$tag_name}')";
+                $sqlQueryTag = mysqli_query($connection, $sqlTag);
+
+                if(!$sqlQueryTag)
+                {
+                    die("MySQL tag query failed!" . mysqlierror($connection));
+                }
+            }
+            
+            
+            $sql = "INSERT INTO blog (date, subject, username, description) VALUES ('{$date}','{$subject}','{$username}', '{$description}')";
+                      
+            // Create mysql query
+            $sqlQuery = mysqli_query($connection, $sql);
+            
+            if(!$sqlQuery)
+            {
+                die("MySQL post query failed!" . mysqli_error($connection));
+            }
+        }
+    }
+    else 
+    {
+        if(empty($subject))
+        {
+            $subject_error = '<div class="alert alert-danger">
+            Subject can not blank</div>';       
+        }
+        if(empty($description))
+        {
+            $description_error = '<div class="alert alert-danger">
+                Description can not be blank.
+                </div>';
+        } 
+        if(empty($tag_name))
+        {
+            $tag_error = '<div class="alert alert-danger">
+                Tags can not be blank.
+            </div>';
+        }
+    }
+
+?>
